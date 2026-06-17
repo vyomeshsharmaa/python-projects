c1de65ccd8ead8c421a719d226fc5d5c2dbd01b4
Vyomeshsharmaa <vyomeshshhh@gmail.com>
2026-06-17 16:29:20 +0530

    add variables section to basics.py

--- basics.py
+++ basics.py
@@ -27,3 +27,49 @@ print("Vyomesh Sharma","18","Haldwani")
 print("Python","Data Science","AI",sep=" | ")
 print("here's your two",end=" ")
 print("joined lines using end")
+
+
+#----VARIABLES----
+
+
+#Creating Variables
+name= "Vyomesh"
+age= 18
+city= "Haldwani"
+is_student= True
+
+
+#using Variables inside print
+print(name)
+print(age)
+print(city)
+print(is_student)
+
+
+#Using vairables with text together
+print("My name is", name)
+print("I am", age, "years old")
+print("I live in", city)
+
+
+#updating a variable
+age=19
+print("Next year I will be", age)
+
+
+#creating a variable from two others
+full_intro= "My name is " + name + " and I live in " + city
+print(full_intro)
+
+
+favourite_language= "Python"  
+print("my fav language is",favourite_language)
+
+my_goal= "To become the greatest and the richest"
+print("my goal is",my_goal)
+
+years_to_master= 2 
+print("I will master Python in",years_to_master,"yrs")
+
+years_to_master=1
+print("or I will try my best to master python in ",years_to_master,"yr")
